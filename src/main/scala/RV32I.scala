ce0e17fbc899115d6e762aeb70dea9b1257de0df
vijayganesh <vijayganeshpc@gmail.com>
2023-02-06 13:14:36 +0530

    Adding Main Scala file

---
+++ src/main/scala/RV32I.scala
@@ -0,0 +1,12 @@
+import chisel3._
+
+/**
+ * This project creates 5 stage RISC V Processor as the part of academic work
+ *
+ *
+ */
+class RV32I extends Module{
+// Main Module for RISC V Processor
+
+
+}
