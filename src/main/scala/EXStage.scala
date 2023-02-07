c1a75b9188b29f5d4bea7b570652875046b280d0
vijayganesh <vijayganeshpc@gmail.com>
2023-02-07 14:53:07 +0530

    Adding Instruction Execute stage

---
+++ src/main/scala/EXStage.scala
@@ -0,0 +1,41 @@
+//import Chisel.switch
+import chisel3._
+import chisel3.util._
+
+/**
+ *
+ * @author P.C. Vijay Ganesh
+ * @todo Finalise the Opcode width
+ * @todo Branch and Shift operations
+ */
+class EXStage (val datawidth : Int = 32) extends Module{
+val IO = IO(new Bundle() {
+  val Opcode = Input(UInt(6.W)) // yet to finalise
+  val Operand1 = Input(UInt(datawidth.W))
+  val Operand2 = Input(UInt(datawidth.W))
+  val ResultAlu = Output(UInt(datawidth.W))
+
+})
+
+  switch(IO.Opcode){
+    is(0.U){
+      IO.ResultAlu := IO.Operand1 + IO.Operand2
+
+    }
+    is(1.U) {
+      IO.ResultAlu := IO.Operand1 - IO.Operand2
+
+    }
+    is(2.U){
+      IO.ResultAlu := IO.Operand1 & IO.Operand2
+    }
+
+    is(3.U) {
+      IO.ResultAlu := IO.Operand1 | IO.Operand2
+    }
+    is(4.U) {
+      IO.ResultAlu := IO.Operand1 ^ IO.Operand2
+    }
+  }
+
+}
