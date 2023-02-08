0118ac22085329d717fb2dcab65bc112663749f4
vijayganesh <vijayganeshpc@gmail.com>
2023-02-08 12:20:23 +0530

    Adding Instruction Execute stage with ALU Seperate class

--- src/main/scala/EXStage.scala
+++ src/main/scala/EXStage.scala
@@ -8,17 +8,32 @@ import chisel3.util._
  * @todo Finalise the Opcode width
  * @todo Branch and Shift operations
  */
-class EXStage (val datawidth : Int = 32) extends Module{
+class EXStage (val datawidth : Int = 32, val idexereg : Int) extends Module{
 val IO = IO(new Bundle() {
-  val Opcode = Input(UInt(6.W)) // yet to finalise
+  val IDEXEReg = Input(UInt(idexereg.W)) // yet to finalise
   val Operand1 = Input(UInt(datawidth.W))
   val Operand2 = Input(UInt(datawidth.W))
   val ResultAlu = Output(UInt(datawidth.W))
 
 })
 
-  switch(IO.Opcode){
-    is(0.U){
+  // from RegIDIE need to decode what to execute
+
+
+
+}
+
+class ALU (val datawidth : Int = 32) extends Module {
+
+  val IO = IO(new Bundle() {
+    val Opcode = Input(UInt(6.W)) // yet to finalise
+    val Operand1 = Input(UInt(datawidth.W))
+    val Operand2 = Input(UInt(datawidth.W))
+    val ResultAlu = Output(UInt(datawidth.W))
+
+  })
+  switch(IO.Opcode) {
+    is(0.U) {
       IO.ResultAlu := IO.Operand1 + IO.Operand2
 
     }
@@ -26,7 +41,7 @@ val IO = IO(new Bundle() {
       IO.ResultAlu := IO.Operand1 - IO.Operand2
 
     }
-    is(2.U){
+    is(2.U) {
       IO.ResultAlu := IO.Operand1 & IO.Operand2
     }
 
@@ -37,5 +52,4 @@ val IO = IO(new Bundle() {
       IO.ResultAlu := IO.Operand1 ^ IO.Operand2
     }
   }
-
 }
